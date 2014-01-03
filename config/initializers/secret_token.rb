b2b46dd91b938f047e781add8d06410dace59528
Jason Benn <jasoncbenn@gmail.com>
2014-01-03 15:44:00 -0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TeamAgents::Application.config.secret_token = '1fe9abadda543894ac036d5cd7deeb3475215985b1514e9ab32d479120448cdd34290e554a635455bc70748ebb5ea21c1fc1493f581099bbaf19a956e8581455'
